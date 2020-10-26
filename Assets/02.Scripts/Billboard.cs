c0fef0b4b42a357e989f1363f64c6c8ee6ab91da
wax9315 <63216589+wax9315@users.noreply.github.com>
2020-10-26 15:40:07 +0900

    30316
    뷁

---
+++ Assets/02.Scripts/Billboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Transform camTr;
+    private Transform tr;
+    void Start()
+    {
+
+        camTr = Camera.main.GetComponent<Transform>();
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tr.LookAt(camTr.position);
+    }
+}
--- Assets/02.Scripts/LookItem.cs
+++ Assets/02.Scripts/LookItem.cs
@@ -5,7 +5,19 @@ using UnityEngine.EventSystems;
 
 public class LookItem : MonoBehaviour,IPointerEnterHandler,
     IPointerExitHandler,IGvrPointerHoverHandler
-{ 
+
+{
+    private Animator anim;
+    private readonly int hashIsOpen = Animator.StringToHash("IsOpen");
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void OnBoxOpen(bool isOpen)
+    {
+        anim.SetBool(hashIsOpen, isOpen);
+    }
     // Update is called once per frame
    public void OnLookItemBox(bool isLookAt)
     {
